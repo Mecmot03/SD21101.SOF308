2ed3f2155440d1834db6980c3bb2c7bc768c5e9f
unknown <nguyenthaigiahu200803@gmail.com>
2026-03-24 21:35:11 +0700

    lab4

---
+++ PS46924_Lab4/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Bai3 from './components/Bai3.vue'
+import Bai4 from './components/Bai4.vue'
+import test from './components/test.vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+
+
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+</script>
+
+<template>
+  <!-- <HelloWorld />
+  <Bai3 />
+  <Bai4 /> -->
+  <test />
+</template>
---
+++ PS46924_Lab4/src/components/test.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1 :class="className" style="font-size: 5rem">Caculator: 15-Giả Hủ</h1>
+<h2 v-html="content" :style="styleData"></h2>
+        <h3>Count {{ count }}</h3>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="reset">Reset</button>
+        <button @click="changeClassName">Change class</button>
+
+        <h2 v-html="count" :style="styleData"></h2>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => { count.value++ 
+styleData.value = "font-size: " + count.value + "rem;"
+}
+const decrement = () => { count.value--
+styleData.value = "font-size: " + count.value + "rem;"
+}
+const reset = () => { count.value = 0
+styleData.value = "font-size: 5rem;"
+}
+
+const content = "xin chào các bạn"
+const className = ref("text-red")
+const styleData = ref("font-size: 5rem;")
+const changeClassName = () => {
+    if (className.value === "text-red")
+        className.value = "text-blue"
+    else {
+        className.value = "text-red"
+    }
+}
+</script>
+
+<style scoped>
+.text-red {
+    color: red;
+}
+
+.text-blue {
+    color: blue;
+}
+</style>
