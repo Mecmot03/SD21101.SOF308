3609bbc85f1a237ab1f4e797f27610492a552dfb
Mecmot03 <nguyenthaigiahu200803@gmail.com>
2026-04-21 11:14:30 +0700

    kiemtrathu

---
+++ kiemtrathu/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Test1 from './components/Test1.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+  <Test1 />
+</template>
---
+++ kiemtrathu/src/components/Test1.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class = "container mt-4">
+        <h1>Bài 1 Counter</h1><br>
+        Counter: <input type="number" v-model="counter" min="0" @input="limit"><br>
+        <button @click="counter++">+</button>
+        <button @click="decrement">-</button>
+        <br>
+        <h1>Bài 2 form nhập thông tin tính tổng tiền</h1><br>
+        Tên sản phẩm: <input type="text" v-model="b2TenSP"><br>
+        Giá sản phẩm: <input type="number" v-model="b2GiaSP"><br>
+        Số lượng: <input type="number" v-model="b2SoLuong" min="0"><br>
+        <h2>Tổng tiền: {{ b2TongTien }}k</h2>
+        <div style="border:1px solid #ccc; padding:10px; margin-top:16px; width:300px;">
+            <h3>Thông tin sản phẩm</h3>
+            <p><strong>Tên sản phẩm:</strong> {{ b2TenSP }}</p>
+            <p><strong>Giá sản phẩm:</strong> {{ b2GiaSP }}k</p>
+            <p><strong>Số lượng:</strong> {{ b2SoLuong }}</p>
+            <p><strong>Tổng tiền:</strong> {{ b2TongTien }}k</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+// Bài 1
+const counter = ref(0);
+
+function limit(e) {
+    if (counter.value < 0) {
+        counter.value = 0;
+    }
+}
+function decrement() {
+    if (counter.value > 0) {
+        counter.value--;
+    }
+}
+
+const b2TenSP = ref('');
+const b2GiaSP = ref(0);
+const b2SoLuong = ref(0);
+const b2TongTien = computed(() => b2GiaSP.value * b2SoLuong.value);
+</script>
+
+
+<style></style>
